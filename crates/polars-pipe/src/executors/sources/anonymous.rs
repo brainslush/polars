6ec3ab1de78502cb743c531ae73afd689ec36985
Benjamin Streitz <benjamin.streitz@pwc.com>
2025-01-12 13:21:03 +0100

    wip

--- crates/polars-pipe/src/executors/sources/anonymous.rs
+++ crates/polars-pipe/src/executors/sources/anonymous.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
-use polars_core::prelude::*;
-use polars_plan::plans::AnonymousScan;
+use polars_core::{prelude::*, POOL};
+use polars_plan::plans::{AnonymousScan, AnonymousScanArgs, AnonymousSourceResult};
 
 use crate::operators::{Source, PExecutionContext, SourceResult};
 
@@ -9,21 +9,33 @@ use crate::operators::{Source, PExecutionContext, SourceResult};
 
 pub struct AnonymousSource {
     pub function: Arc<dyn AnonymousScan>,
+    n_threads: usize,
+    scan_arguments: AnonymousScanArgs
 }
 
 impl AnonymousSource {
-    pub fn new(function: Arc<dyn AnonymousScan>) -> Self {
-        Self { function }
+    pub fn new(
+        function: Arc<dyn AnonymousScan>,
+    ) -> Self {
+        Self { function, n_threads: POOL.current_num_threads(), }
+    }
+}
+
+impl Into<SourceResult> for AnonymousSourceResult {
+    fn into(self) -> SourceResult {
+        match self {
+            AnonymousSourceResult::Finished => SourceResult::Finished,
+            AnonymousSourceResult::GotMoreData(chunks) => SourceResult::GotMoreData(chunks),
+        }
     }
 }
 
 impl Source for AnonymousSource {
     fn get_batches(&mut self, _context: &PExecutionContext) -> PolarsResult<SourceResult> {
-        match self.function.get_batches(scan_opts)? {
-        
-        }
+        self.function.get_batches(scan_opts)
     }
+
     fn fmt(&self) -> &str {
-        self.function.fmt()
+        self.function.as_ref().fmt()
     }
 }
